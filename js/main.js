fde6bb5f41de2f97cc8b323b6611fad51f098804
robpap96 <robertopapagni1996@outlook.it>
2022-11-15 19:44:14 +0100

    problemi col binding

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const { createApp } = Vue
   createApp({
     data() {
         return {
-            newTodo: '',
+            newTask: '',
             tasks: [
                 'andare dal medico',
                 'comprare la dro',
@@ -14,13 +14,38 @@ const { createApp } = Vue
         }
     },
     methods: {
-        addTask(task) {
-            this.tasks.push(task);
-            this.newTodo = '';
+        addTask() {
+            const objNewTask = {
+                text: this.newTask,
+                done: false,
+            }
+            this.tasks.push(this.newTask);
+            this.newTask = '';
         }, 
         removeTask(task){
             this.tasks.splice(task, 1);
         },
+        wellDone(){
+            if(this.done === false){
+                this.done = true;
+            } else {
+                this.done = false;
+            }
+
+        },
+        isDone(task){
+            if(task.done === true){
+                //debug
+                console.log(task.done);
+                return true;
+            } else {
+                //debug
+                console.log(task.done);
+                return false;
+            }
+
+        }
+    
     }
     
   }).mount('#app')
